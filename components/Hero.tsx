771133fe86041007ac63f1cfabd61946bb938c59
clement bastien bastienclement826@gmail.com <bastienclement826@gmail.com>
2024-02-29 14:54:28 +0100

    ll

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import ParticlesBackground from "./ParticlesBackground";
-import { FaLinkedin, FaSquareXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
 import Link from "next/link";
 
